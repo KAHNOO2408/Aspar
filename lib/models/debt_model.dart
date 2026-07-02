b6ed3e2caa8a5d20292971e8b01d6e5ff8ff8996
KAHNOO2408 <kahnoo9203@gmail.com>
2026-07-02 13:32:34 +0330

    Update debt_model.dart

--- lib/models/debt_model.dart
+++ lib/models/debt_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../database/db_helper.dart';
 
 enum DebtType { owed, receivable }
 
@@ -11,7 +12,6 @@ class Debt {
   final DateTime date;
   final DebtType type;
   double paidAmount;
-
   Debt({
     this.id,
     required this.personName,
@@ -22,9 +22,7 @@ class Debt {
     required this.type,
     this.paidAmount = 0,
   });
-
   double get remainder => totalAmount - paidAmount;
-
   Map<String, dynamic> toMap() {
     return {
       'id': id,
@@ -37,17 +35,16 @@ class Debt {
       'paidAmount': paidAmount,
     };
   }
-
   factory Debt.fromMap(Map<String, dynamic> map) {
     return Debt(
       id: map['id'],
       personName: map['personName'],
       personFamily: map['personFamily'],
-      totalAmount: map['totalAmount'],
+      totalAmount: (map['totalAmount'] as num).toDouble(),
       description: map['description'],
       date: DateTime.parse(map['date']),
       type: map['type'] == 'owed' ? DebtType.owed : DebtType.receivable,
-      paidAmount: map['paidAmount'] ?? 0,
+      paidAmount: (map['paidAmount'] ?? 0 as num).toDouble(),
     );
   }
 }
@@ -55,50 +52,64 @@ class Debt {
 class DebtProvider extends ChangeNotifier {
   List<Debt> debts = [];
 
-  void addDebt(Debt debt) {
-    debt.id == null ? debts.add(Debt(
-      id: debts.isEmpty ? 1 : debts.last.id! + 1,
-      personName: debt.personName,
-      personFamily: debt.personFamily,
-      totalAmount: debt.totalAmount,
-      description: debt.description,
-      date: debt.date,
-      type: debt.type,
-      paidAmount: 0,
-    )) : debts.add(debt);
+  DebtProvider() {
+    loadDebts();
+  }
+
+  Future<void> loadDebts() async {
+    debts = await DatabaseHelper.getDebts();
     notifyListeners();
   }
 
-  void payDebt(int id, double amount) {
-    final debt = debts.firstWhere((d) => d.id == id);
+  Future<void> addDebt(Debt debt) async {
+    final debtToSave = debt.id == null
+        ? Debt(
+            id: DateTime.now().millisecondsSinceEpoch,
+            personName: debt.personName,
+            personFamily: debt.personFamily,
+            totalAmount: debt.totalAmount,
+            description: debt.description,
+            date: debt.date,
+            type: debt.type,
+            paidAmount: debt.paidAmount,
+          )
+        : debt;
+    await DatabaseHelper.insertDebt(debtToSave);
+    await loadDebts();
+  }
+
+  Future<void> payDebt(int id, double amount) async {
+    final index = debts.indexWhere((d) => d.id == id);
+    if (index == -1) return;
+    final debt = debts[index];
     if (debt.paidAmount + amount <= debt.totalAmount) {
       debt.paidAmount += amount;
-      notifyListeners();
+      await DatabaseHelper.updateDebt(debt);
+      await loadDebts();
     }
   }
 
-  void refundDebt(int id, double amount) {
-    final debt = debts.firstWhere((d) => d.id == id);
+  Future<void> refundDebt(int id, double amount) async {
+    final index = debts.indexWhere((d) => d.id == id);
+    if (index == -1) return;
+    final debt = debts[index];
     if (debt.paidAmount >= amount) {
       debt.paidAmount -= amount;
-      notifyListeners();
+      await DatabaseHelper.updateDebt(debt);
+      await loadDebts();
     }
   }
 
-  void deleteDebt(int id) {
-    debts.removeWhere((d) => d.id == id);
-    notifyListeners();
+  Future<void> deleteDebt(int id) async {
+    await DatabaseHelper.deleteDebt(id);
+    await loadDebts();
   }
 
   double getTotalOwed(DateTime? startDate, DateTime? endDate) {
-    return debts
-        .where((d) => d.type == DebtType.owed)
-        .fold(0.0, (sum, d) => sum + d.remainder);
+    return debts.where((d) => d.type == DebtType.owed).fold(0.0, (sum, d) => sum + d.remainder);
   }
 
   double getTotalReceivable(DateTime? startDate, DateTime? endDate) {
-    return debts
-        .where((d) => d.type == DebtType.receivable)
-        .fold(0.0, (sum, d) => sum + d.remainder);
+    return debts.where((d) => d.type == DebtType.receivable).fold(0.0, (sum, d) => sum + d.remainder);
   }
 }
